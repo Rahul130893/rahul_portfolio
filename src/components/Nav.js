ea3317f0d879f65ab4fe5c07ac9f0a521b7b4ca3
Rahul130893 <93374760+Rahul130893@users.noreply.github.com>
2022-04-30 02:41:39 +0530

    added

---
+++ src/components/Nav.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import {
+  FaGithubSquare,
+  FaLinkedin,
+  // FaTwitterSquare,
+  FaEnvelope,
+  FaTimes,
+  FaBars,
+} from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+function Nav() {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  return (
+    <div className="navbar">
+      <Link to='/'>
+      <div className="logo">{'<Rahul/>'}</div>
+      </Link>
+      <div className={click ? 'menu active' : 'menu'}>
+        <ul className="navvs">
+          <li onClick={closeMobileMenu}><a href="#aboutDiv"> About</a></li>
+          <li onClick={closeMobileMenu}><a href="#projectDiv">Projects</a> </li>
+
+          <li onClick={closeMobileMenu}> <a href="#bottom">Contact</a> </li>
+
+          <li className="navvs" onClick={closeMobileMenu}>
+            <a target="_blank" rel="noreferrer" href={"https://drive.google.com/file/d/1MvbMPI1hyIIasbDpngjUoiPfMWdD4u_S/view?usp=sharing"}>
+              Resume
+            </a>
+          </li>
+        </ul>
+        <div className="socials">
+          <ul>
+            <li className="resumes" onClick={closeMobileMenu}>
+              {/* <Link to='/blog'>
+                <div>Blogs</div>
+              </Link> */}
+            </li>
+            <li onClick={closeMobileMenu}>
+              <a title='rahulrdx1308@gmail.com'
+                target="_blank"
+                rel="noreferrer"
+                href="mailto:rahulrdx1308@gmail.com"
+              >
+                <FaEnvelope />{' '}
+              </a>
+            </li>
+            <li onClick={closeMobileMenu}>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/Rahul130893"
+              >
+                <FaGithubSquare />
+              </a>
+            </li>
+            <li onClick={closeMobileMenu}>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.linkedin.com/in/rahul-kumar-61436b233/"
+              >
+                <FaLinkedin />
+              </a>
+            </li>
+            {/* <li onClick={closeMobileMenu}>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://twitter.com/a0x001"
+              >
+                <FaTwitterSquare />
+              </a>
+            </li> */}
+          </ul>
+        </div>
+      </div>
+      <div className="mobile-menu" onClick={handleClick}>
+        {click ? <FaTimes /> : <FaBars />}
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
+// menu
